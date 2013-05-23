8d6cb87146775622d19e52057424695448619616
maul.esel <maul.esel@go4more.de>
2013-05-23 18:14:50 +0200

    add classes for schema nodes: attributes and elements.

---
+++ XmlSchemaNode.php
@@ -0,0 +1,46 @@
+<?php
+require_once dirname(__FILE__) . '/XmlSchemaType.php';
+
+abstract class XmlSchemaNode {
+	protected $schema;
+	protected $elem;
+	protected $name;
+	protected $type = NULL;
+
+	public function __construct($schema, $element) {
+		$this->elem = $element;
+		$this->schema = $schema;
+
+		$this->name = $element->getAttribute('name');
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getType() {
+		if ($this->type !== NULL) {
+			return $this->type;
+		}
+
+		if ($this->elem->hasAttribute('type')) {
+			$type_name = $this->elem->getAttribute('type');
+			return $this->type = $this->schema->getType($type_name);
+		}
+
+		$nodes = $this->schema->xpath->query('simpleType', $this->elem);
+		if ($nodes->length > 0) {
+			$node = $nodes->item(0);
+			return $this->type = new XmlSchemaSimpleType('<# anonymous type #>', $node);
+		}
+
+		$nodes = $this->schema->xpath->query('complexType', $this->elem);
+		if ($nodes->length > 0) {
+			$node = $nodes->item(0);
+			return $this->type = new XmlSchemaComplexType('<# anonymous type #>', $node);
+		}
+
+		throw new Exception('No type found for this node');
+	}
+}
+?>
