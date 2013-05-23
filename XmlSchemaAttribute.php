5c412f1ca8bbe2b427165991318d1ee181d914ee
maul.esel <maul.esel@go4more.de>
2013-05-23 21:15:43 +0200

    add isRequired() to XmlSchemaAttribute

--- XmlSchemaAttribute.php
+++ XmlSchemaAttribute.php
@@ -2,5 +2,8 @@
 require_once dirname(__FILE__) . '/XmlSchemaNode.php';
 
 class XmlSchemaAttribute extends XmlSchemaNode {
+	public function isRequired() {
+		return $this->elem->getAttribute('use') == 'required';
+	}
 }
 ?>
