1fe900a7674b957278b9eb9e93cb9baaa39eb8fb
maul.esel <maul.esel@go4more.de>
2013-06-30 22:29:14 +0200

    add dummy compositor classes

---
+++ compositors/XmlSchemaCompositor.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(__FILE__) . '/../XmlSchemaParticle.php';
+
+abstract class XmlSchemaCompositor extends XmlSchemaParticle {
+}
+?>
---
+++ compositors/XmlSchemaSequence.php
@@ -0,0 +1,8 @@
+<?php
+require_once dirname(__FILE__) . '/XmlSchemaCompositor.php';
+
+class XmlSchemaSequence extends XmlSchemaCompositor {
+	public function coerce($value, $parent) {
+	}
+}
+?>
