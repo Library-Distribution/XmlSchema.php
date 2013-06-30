6f4169d1547c223d99c74e4377640909a7b1464e
maul.esel <maul.esel@go4more.de>
2013-06-30 22:30:55 +0200

    begin working on complex type coercion

--- types/XmlSchemaComplexType.php
+++ types/XmlSchemaComplexType.php
@@ -1,13 +1,108 @@
 <?php
 require_once dirname(__FILE__) . '/XmlSchemaType.php';
 
+require_once dirname(__FILE__) . '/../compositors/XmlSchemaAll.php';
+require_once dirname(__FILE__) . '/../compositors/XmlSchemaChoice.php';
+require_once dirname(__FILE__) . '/../compositors/XmlSchemaSequence.php';
+
 class XmlSchemaComplexType extends XmlSchemaType {
+
+	const CT_NONE = 0;
+	const CT_GROUP = 1;
+
+	const CT_ALL = 2;
+	const CT_CHOICE = 3;
+	const CT_SEQUENCE = 4;
+
+	const CT_SIMPLECONTENT = 5;
+	const CT_COMPLEXCONTENT = 6;
+
+	protected $content = NULL;
+	protected $attributes = NULL;
+
 	public function __construct($name, $schema, $node) {
 		parent::__construct($name, $schema, $node);
+
+		$content_node = $this->getContentNode();
+		$content_type = $this->getContentType();
+
+		if ($content_type != self::CT_SIMPLECONTENT && $content_type != self::CT_COMPLEXCONTENT) {
+			$this->parseAttributes();
+		}
+
+		switch ($content_type) {
+			case self::CT_ALL:      $this->content = new XmlSchemaAll($content_node, $schema);
+			case self::CT_CHOICE:   $this->content = new XmlSchemaChoice($content_node, $schema);
+			case self::CT_SEQUENCE: $this->content = new XmlSchemaSequence($content_node, $schema);
+		}
 	}
 
 	public function coerce($value, $doc, $schema_node) {
-		throw new Exception('Cannot coerce for complex type');
+		$node = $doc->createElement($schema_node->getName());
+		$content_type = $this->getContentType();
+
+		if ($content_type != self::CT_SIMPLECONTENT && $content_type != self::CT_COMPLEXCONTENT) {
+			foreach ($this->attributes AS $attribute) {
+				$attr_name = $attribute->getName();
+
+				if (is_array($value) && isset($value[$attr_name])) {
+					$node->appendChild($attribute->getType()->coerce($value[$attr_name], $doc, $attribute));
+				} else if ($attribute->isRequired()) {
+					throw new Exception('Required attribute is missing');
+				}
+			}
+		}
+
+		if ($content_type !== self::CT_NONE) {
+			$this->content->coerce($value, $node);
+		}
+
+		return $node;
+	}
+
+	private function parseAttributes() {
+		$this->attributes = array();
+		foreach ($this->schema->query('./xsd:attribute', $this->node) AS $attr_node) {
+			$attribute = new XmlSchemaAttribute($this->schema, $attr_node);
+			$attributes[] = $attribute;
+
+			if (is_array($value) && isset($value[$attr_name])) {
+				$this->appendChild($attribute->getType()->coerce($value[$attr_name], $doc, $attr_node));
+
+			} else if ($attribute->isRequired()) {
+				throw new Exception('Required attribute is missing');
+			}
+		}
+	}
+
+	private $content_node = NULL;
+
+	private function getContentNode() {
+		if ($this->content_node === NULL) {
+			$list = $this->schema->query('xsd:group|xsd:sequence|xsd:all|xsd:choice|xsd:simpleContent|xsd:complexContent', $this->node);
+			if ($list->length == 0) {
+				return NULL;
+			}
+			$this->content_node = $list->item(0);
+		}
+		return $this->content_node;
+	}
+
+	private function getContentType() {
+		$node = $this->getContentNode();
+		if ($node === NULL) {
+			return self::CT_NONE;
+		}
+
+		switch ($node->tagName) {
+			case 'xsd:group':          return self::CT_GROUP;
+			case 'xsd:all':            return self::CT_ALL;
+			case 'xsd:choice':         return self::CT_CHOICE;
+			case 'xsd:sequence':       return self::CT_SEQUENCE;
+			case 'xsd:simpleContent':  return self::CT_SIMPLECONTENT;
+			case 'xsd:complexContent': return self::CT_COMPLEXCONTENT;
+		}
+		throw new Exception('Unknown content type');
 	}
 }
 ?>
